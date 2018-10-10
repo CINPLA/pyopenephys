89421fd5eeeb91cef11e109f94a2ae4ea4e45ac4
Alessio Buccino <alejoe9187@gmail.com>
2018-10-10 21:07:56 +0200

    fixed scipy dependency

--- setup.py
+++ setup.py
@@ -1,33 +1,31 @@
 from setuptools import setup, find_packages
-import pyopenephys
+# import pyopenephys
 
 long_description = open("README.md").read()
 
 entry_points = None
 
-install_requires = []
-
 setup(
     name="pyopenephys",
-    version=pyopenephys.__version__,
+    version='0.1.1',
     author="Alessio Buccino",
     author_email="alessiob@ifi.uio.no",
     description="Python package for parsing Open Ephys data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/CINPLA/py-open-ephys",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
     packages=find_packages(),
     entry_points=entry_points,
     include_package_data=True,
     install_requires=[
         'numpy',
+        'scipy',
         'quantities',
         'xmltodict',
-        'scipy'
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ]
 )

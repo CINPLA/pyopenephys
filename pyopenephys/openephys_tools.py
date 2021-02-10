352db59ee5b8e18e589987f0da80743384e5b05b
luiz <luiz_tauffer@hotmail.com>
2021-02-10 13:57:26 +0100

    float option as string

--- pyopenephys/openephys_tools.py
+++ pyopenephys/openephys_tools.py
@@ -18,7 +18,7 @@ MAX_NUMBER_OF_RECORDS = int(1e6)
 MAX_NUMBER_OF_EVENTS = int(1e6)
 
 def loadContinuous(filepath, dtype='int16'):  
-    assert dtype in (float, 'int16'), \
+    assert dtype in ('float', 'int16'), \
         'Invalid data type specified for loadContinous, valid types are float and int16'
 
     # print("Loading continuous data...")
@@ -58,7 +58,7 @@ def loadContinuous(filepath, dtype='int16'):
 
         recordingNumbers[recordNumber] = (np.fromfile(f, np.dtype('>u2'), 1))  # big-endian 16-bit unsigned integer
 
-        if dtype == float:  # Convert data to float array and convert bits to voltage.
+        if dtype == 'float':  # Convert data to float array and convert bits to voltage.
             data = np.fromfile(f, np.dtype('>i2'), N) * float(header['bitVolts'])  # big-endian 16-bit signed integer, multiplied by bitVolts
         else:  # Keep data in signed 16 bit integer format.
             data = np.fromfile(f, np.dtype('>i2'), N)  # big-endian 16-bit signed integer
